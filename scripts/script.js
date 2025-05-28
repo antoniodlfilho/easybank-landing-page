6d142df73529f879f815a80cf667f36a06cb4c10
Antonio Filho <antoniofilhosato@gmail.com>
2025-05-28 15:07:37 -0300

    versão mobile e ajustes

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const menuHam = document.getElementById("menu_mob");
+const listMenuMobile = document.getElementById("active_menu_mob");
+const main = document.getElementById("main_bg");
+
+function activeMenu() {
+  listMenuMobile.classList.toggle("active");
+  listMenuMobile.classList.toggle("bg-menu");
+}
+
+function imgTroca() {
+  if (listMenuMobile.classList.contains("active")) {
+    menuHam.src = "./images/icon-close.svg";
+  } else {
+    menuHam.src = "./images/icon-hamburger.svg";
+    // listMenuMobile.classList.remove("bg-main");
+  }
+}
+
+menuHam.addEventListener("click", activeMenu);
+menuHam.addEventListener("click", imgTroca);
